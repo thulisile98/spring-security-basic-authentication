53856187b4243bc9068f32ad2c9630db28cf9767
Angel Mashego <thulisile.ma98@gmail.com>
2023-12-27 13:57:48 +0200

    security configuration using basic authentication

---
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,71 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DemoSecurityConfig {
+
+
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+
+
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//
+//        UserDetails tana = User.builder()
+//                .username("tana")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(john,mary,tana);
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/****").hasRole("ADMIN")
+        );
+
+        //USING BASIC AUTHENTICATION
+
+        http.httpBasic(Customizer.withDefaults());
+
+       //disabble CSRF BCZZ NOT REQUIRED  FOR STATELESS REST APIs that use put, delete, post ..
+
+        http.csrf(csrf->csrf.disable());
+
+        return  http.build();
+    }
+}
